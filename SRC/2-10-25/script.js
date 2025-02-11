12111fb73ade7de15f318508bd968cea2557d138
marcmathews867 <48696663+marcmathews867@users.noreply.github.com>
2025-02-10 20:58:57 -0500

    end of class 2-10-25
    html and JS interactive button example

---
+++ SRC/2-10-25/script.js
@@ -0,0 +1,22 @@
+
+
+function Question(){
+    let response = document.getElementById("response").value;
+    if(response == "true"){
+        alert("Correct!");
+        }
+        
+        else{
+            alert("Wrong!")
+        }
+    
+}
+
+/*
+  Simple Quiz App
+  Ask a basic question with a one-word answer. (<p> of html)
+  use a text input to get the user's answer when a button is pressed.
+  The button should call a function that checks if the answer is correct.
+  The function should alert the user if it is right or wrong.
+*/
+
